7086fbaa39e853b323c8a6ba0cf00aacb130a42d
chabotd <devnull@localhost>
2008-09-17 13:37:58 -0600

    added files to rtems/mainApp

---
+++ rtems/mainApp/PSController.c
@@ -0,0 +1,73 @@
+/*
+ * PSController.c
+ *
+ *  Created on: Sep 15, 2008
+ *      Author: chabotd
+ */
+#include <stdint.h>
+
+#include <sis1100_api.h>
+#include <utils.h> /*usecSpinDelay()*/
+#include <psDefs.h>
+
+void UpdateSetPoint(int fd, uint32_t vmeAddr, uint32_t channel, uint32_t setpoint) {
+	int dacSetPoint;
+    uint32_t value = 0;
+    int rc;
+
+
+    dacSetPoint = setpoint * DAC_AMP_CONV_FACTOR;
+
+	value = (channel & PS_CHANNEL_MASK) << PS_CHANNEL_OFFSET;
+    value = value | (dacSetPoint & 0xFFFFFF);
+
+    /* write the 32 bits out to the power supply*/
+    //VmeWrite_32(mod,vmeAddr,value);
+    rc=vme_A24D32_write(fd,vmeAddr,value);
+    if(rc) {
+    	syslog(LOG_INFO, "SetPwrSupply: failed VME write--%#x\n",rc);
+    	return;
+    }
+    /* added for Milan G IE Power 04/08/2002*/
+    usecSpinDelay(30);
+
+    /* toggle the PS_LATCH bit */
+    //VmeWrite_32(mod,vmeAddr,(value | PS_LATCH));
+    rc=vme_A24D32_write(fd,vmeAddr,(value | PS_LATCH));
+    if(rc) {
+    	syslog(LOG_INFO, "SetPwrSupply: failed VME write--%#x\n",rc);
+    	return;
+    }
+    /* wait some time */
+    usecSpinDelay(7);
+
+    /* drop the PS_LATCH bit and data bits */
+    //VmeWrite_32(mod,vmeAddr,0x00000000);
+    rc=vme_A24D32_write(fd,vmeAddr,0x00000000);
+    if(rc) {
+    	syslog(LOG_INFO, "SetPwrSupply: failed VME write--%#x\n",rc);
+    	return;
+    }
+	/* wait some time */
+	usecSpinDelay(7);
+}
+
+void ToggleUpdateBit(int fd, uint32_t vmeAddr, uint32_t channel, uint32_t setpoint) {
+	/* raise the UPDATE bit */
+    //VmeWrite_32(mod,vmeAddr,UPDATE);
+	rc=vme_A24D32_write(fd,vmeAddr,UPDATE);
+    if(rc) {
+    	syslog(LOG_INFO, "SetPwrSupply: failed VME write--%#x\n",rc);
+    	return;
+    }
+    /* wait some time */
+    usecSpinDelay(7);
+
+    /* drop the UPDATE bit */
+	//VmeWrite_32(mod,vmeAddr,0x00000000);
+    rc=vme_A24D32_write(fd,vmeAddr,0x00000000);
+	if(rc) {
+    	syslog(LOG_INFO, "SetPwrSupply: failed VME write--%#x\n",rc);
+    	return;
+    }
+}
---
+++ rtems/mainApp/psDefs.h
@@ -0,0 +1,27 @@
+/*
+ * psDefs.h
+ *
+ *  Created on: Sep 15, 2008
+ *      Author: chabotd
+ */
+
+#ifndef PSDEFS_H_
+#define PSDEFS_H_
+
+/*#define DAC_AMP_CONV_FACTOR 42234.31*/
+#define DAC_AMP_CONV_FACTOR 1
+
+#define OUTPUT_REG_OFFSET 0x08
+#define INPUT_REG_OFFSET 0x04
+#define CONTROL_REG_OFFSET 0x02
+
+#define PS_CHANNEL_OFFSET 24
+#define PS_CHANNEL_MASK 0xF
+
+#define PS_LATCH 0x20000000
+#define DROP_PS_LATCH 0xDFFFFFFF
+
+#define UPDATE 0x80000000
+#define DROP_UPDATE 0x7FFFFFFF
+
+#endif /* PSDEFS_H_ */
